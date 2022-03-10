9e545bb51fc3b051f0846cb74c643aec1666b90b
Cesar Lizcano <cliscano@adintelo.com>
2022-03-10 18:24:42 -0500

    DocumentType * view * api - provider

---
+++ lib/src/models/document_type_model.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+DocumentTypeModel documentModelFromJson(String str) => DocumentTypeModel.fromJson(json.decode(str));
+
+String documentModelToJson(DocumentTypeModel data) => json.encode(data.toJson());
+
+class DocumentTypeModel {
+  List<DocumentType> documentsType;
+
+  DocumentTypeModel({
+    this.documentsType,
+  });
+
+  factory DocumentTypeModel.fromJson(Map<String, dynamic> json) => DocumentTypeModel(
+    documentsType: List<DocumentType>.from(json["data"].map((x) => DocumentType.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "data": List<dynamic>.from(documentsType.map((x) => x.toJson())),
+  };
+
+}
+
+class DocumentType {
+  String id;
+  String name;
+  String description;
+
+  DocumentType({
+    this.id,
+    this.name,
+    this.description,
+  });
+
+  factory DocumentType.fromJson(Map<String, dynamic> json) => DocumentType(
+    id: json["id"].toString(),
+    name: json["docuement_type"],
+    description: json["description"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "docuement_type": name,
+    "description": description
+  };
+
+  @override
+  String toString() {
+    return 'FeedDocument{id: $id, name: $name, description: $description}';
+  }
+}
